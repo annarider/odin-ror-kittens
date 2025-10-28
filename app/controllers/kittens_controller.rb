ab7493f73948d98ffd69dc6c07726bcd3f5e76e6
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-28 12:24:38 -0600

    feat: add KittensController#create action logic
    - allow create action
    - add strong params with private method

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -11,5 +11,19 @@ class KittensController < ApplicationController
     @kitten = Kitten.new
   end
 
-  
+  def create
+    @kitten = Kitten.build(kitten_params)
+
+    if @kitten.save
+      redirect_to kittens_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
