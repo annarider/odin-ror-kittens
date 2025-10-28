c508d9f1d1678c135875b44e5c4a525ce806c044
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-28 08:51:07 -0600

    feat: add validations to db migration

--- db/migrate/20251028144914_create_kittens.rb
+++ db/migrate/20251028144914_create_kittens.rb
@@ -1,8 +1,8 @@
 class CreateKittens < ActiveRecord::Migration[8.0]
   def change
     create_table :kittens do |t|
-      t.string :name
-      t.integer :age
+      t.string :name, null: false
+      t.integer :age, null: false
       t.string :cuteness
       t.integer :softness
 
