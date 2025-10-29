853d5d0b93ddd560d5f8f9691b1cc2d6c1bfe581
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-29 09:53:24 -0600

    feat: add flash messages to KittensController

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,8 +15,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.build(kitten_params)
 
     if @kitten.save
+      flash[:notice] = 'New kitten added! Welcome to the cute family ☀️'
       redirect_to kittens_path
     else
+      flash.now[:alert] = 'Oops! Your kitten form has errors.'
       render :new, status: :unprocessable_entity
     end
   end
@@ -29,8 +31,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
+      flash[:success] = 'Kitty got an update! Even more purr-fect now.'
       redirect_to @kitten
     else
+      flash.now[:error] = 'Nice try but your form has problems'
       render :edit, status: :unprocessable_entity
     end
   end
@@ -39,6 +43,7 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
 
+    flash[:notice] = 'Bye bye kitty.'
     redirect_to root_path, status: :see_other
   end
 
