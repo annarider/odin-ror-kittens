4d57fd07f9395c960845acdcb09c34528ea3bc5e
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-29 10:46:36 -0600

    style: use rubocop to fix style problems

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -25,9 +25,9 @@ class KittensController < ApplicationController
     @kitten = Kitten.build(kitten_params)
 
     if @kitten.save
-      redirect_to kittens_path, notice: 'New kitten added! Welcome to the cute family ☀️'
+      redirect_to kittens_path, notice: "New kitten added! Welcome to the cute family ☀️"
     else
-      flash.now[:alert] = 'Oops! Your kitten form has errors.'
+      flash.now[:alert] = "Oops! Your kitten form has errors."
       render :new, status: :unprocessable_entity
     end
   end
@@ -40,9 +40,9 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
-      redirect_to @kitten, notice: 'Kitty got an update! Even more purr-fect now.'
+      redirect_to @kitten, notice: "Kitty got an update! Even more purr-fect now."
     else
-      flash.now[:alert] = 'Nice try but your form has problems'
+      flash.now[:alert] = "Nice try but your form has problems"
       render :edit, status: :unprocessable_entity
     end
   end
@@ -51,7 +51,7 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
 
-    redirect_to root_path, status: :see_other, notice: 'Bye bye kitty.'
+    redirect_to root_path, status: :see_other, notice: "Bye bye kitty."
   end
 
   private
